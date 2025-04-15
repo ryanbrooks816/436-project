1a2995e2febc730d7d0b2462b5e9dc704a24e7b4
connorstubbs <connor_stubbs@uri.edu>
2025-04-15 14:56:52 -0400

    User profile and change password pagesadded. navbar.php modified to not start a session if one is started and use absolute paths.

--- classes/navbar.php
+++ classes/navbar.php
@@ -1,6 +1,8 @@
 <?php
+if (session_status() === PHP_SESSION_NONE) {
+  session_start();
+}
 require_once 'db.php';
-session_start();
 
 // Check if the user is logged in
 if (!isset($_SESSION['email'])) {
@@ -19,22 +21,22 @@ $profilePicData = $customer && $customer['profile_picture']
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm mb-4">
   <div class="container-fluid">
-    <h1 class="navbar-brand text-primary" style="font-size: 2rem;">Accessible Games Support Center</h1> <!-- Changed to h1 -->
+    <h1 class="navbar-brand text-primary" style="font-size: 2rem;">Accessible Games Support Center</h1>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav mr-auto mb-2 mb-lg-0">
         <li class="nav-item ml-5">
-          <a class="nav-link" href="game_search.php" style="font-size: 1.5rem;">Game Search</a>
+          <a class="nav-link" href="/game_search.php" style="font-size: 1.5rem;">Game Search</a>
         </li>
         <li class="nav-item ml-5">
-          <a class="nav-link" href="tickets.php" style="font-size: 1.5rem;">Tickets</a>
+          <a class="nav-link" href="/tickets.php" style="font-size: 1.5rem;">Tickets</a>
         </li>
       </ul>
       <ul class="navbar-nav ml-auto">
         <li class="nav-item">
-          <a class="nav-link" href="profile.php">
+          <a class="nav-link" href="/user-profile/profile.php">
             <img src="<?php echo htmlspecialchars($profilePicData); ?>" alt="Profile" width="50" height="50" class="rounded-circle">
           </a>
         </li>
