b6df0f74efc8bfe8736634c8034fee79aa2a6a9e
Ryan Brooks <brooksryan1234@gmail.com>
2025-04-23 00:32:36 -0400

    Added 403 and 404 pages

---
+++ 403.php
@@ -0,0 +1,10 @@
+<?php require "header.php" ?>
+
+<div class="container text-center mt-5">
+    <h1 class="display-1 text-danger">403</h1>
+    <h2 class="mb-4">Access Denied</h2>
+    <p class="lead">Sorry, you do not have permission to access this page.</p>
+    <a href="index.php" class="btn btn-primary mt-3">Go to Homepage</a>
+</div>
+
+<?php require "footer.php" ?>
