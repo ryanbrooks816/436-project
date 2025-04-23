e4b81fb88441e5223723d8d7bc7834cf0bf9a03d
Ryan Brooks <brooksryan1234@gmail.com>
2025-04-23 00:33:17 -0400

    Added error reporting to config.php

--- classes/config.php
+++ classes/config.php
@@ -1,4 +1,9 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('log_errors', 1); // Enable error logging
+ini_set('error_log', __DIR__ . '/error.log'); // Set the error log file path
+
 require_once 'db.php';
 
 $sql = "SELECT recaptcha_secret, recaptcha_site FROM config LIMIT 1";
@@ -8,5 +13,6 @@ if ($config) {
     $recaptcha_secret = $config['recaptcha_secret'];
     $recaptcha_site = $config['recaptcha_site'];
 } else {
+    error_log("Configuration not found.");
     die("Configuration not found.");
 }
