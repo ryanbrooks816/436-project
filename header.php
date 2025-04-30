c3969a530e160290a755e1bac6a8634e7407b240
Matthew Connors <99858739+mconnors55@users.noreply.github.com>
2025-04-29 23:57:50 -0400

    Update header.php

--- header.php
+++ header.php
@@ -9,46 +9,6 @@
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
 
-    <style>
-    body {
-      min-height: 100vh;
-      display: flex;
-      flex-direction: column;
-      padding-top: 100px;
-    }
-    .sidebar {
-      width: 190px;
-      height: 100vh;
-      position: fixed;
-      top: 0;
-      left: 0;
-      background-color: #343a40;
-      padding-top: 140px;
-    }
-    .sidebar a {
-      padding: 15px;
-      text-decoration: none;
-      font-size: 18px;
-      color: #ccc;
-      display: block;
-    }
-    .sidebar a:hover {
-      background-color: #495057;
-      color: white;
-    }
-    .content {
-      margin-left: 250px;
-      padding: 20px;
-      flex-grow: 1;
-    }
-    .navbar {
-      z-index: 1001;
-    }
-    #main-content {
-      padding-left: 250px;
-    }
-  </style>
-
 </head>
 
 <body>
